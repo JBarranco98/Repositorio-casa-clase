ecbbf6832e3686f138b2b902e0d36205cdf5a28f
JBarranco98 <jorgembg98@gmail.com>
2022-10-28 19:30:32 +0200

    28 octubre

---
+++ Proyecto prueba/Assets/Ejercicios/28 octubre/GunScript.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunScript : MonoBehaviour
+{
+    public Transform MyCamera;
+
+    public float range = 100f;
+
+    public GameObject bulletImpactPrefab;
+
+    public float impactForce = 20f;
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        RaycastHit hitInfo;
+        if (Physics.Raycast(MyCamera.position, MyCamera.forward, out hitInfo, range))
+        {
+            GameObject bulletImpact = Instantiate(bulletImpactPrefab, hitInfo.point, Quaternion.LookRotation(hitInfo.normal));
+            Destroy(bulletImpact, 2);
+
+            Rigidbody rb = hitInfo.collider.gameObject.GetComponent<Rigidbody>();
+
+            if (rb != null)
+            {
+                rb.AddForce(-hitInfo.normal * impactForce, ForceMode.Impulse);
+            }
+        }
+        else
+        {
+            Debug.Log("Apunta mejor crack ;)");
+        }
+    }
+}

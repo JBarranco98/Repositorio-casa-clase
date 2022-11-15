336644ae50411e8a9926440bab62db790f1ebd5a
JBarranco98 <jorgembg98@gmail.com>
2022-11-15 20:18:00 +0100

    15 noviembre

--- Proyecto prueba/Assets/Scripts/EnemyHealth.cs
+++ Proyecto prueba/Assets/Scripts/EnemyHealth.cs
@@ -1,11 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyHealth : Health
 {
+    private Animator myAnimator;
+    private NavMeshAgent agent;
+    public float timeToDie = 4;
+    private Collider myCollider;
+
+    private void Awake()
+    {
+        myAnimator = GetComponentInChildren<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+        myCollider = GetComponent<Collider>();
+    }
+
     public override void Die()
     {
+        myCollider.enabled = false;
+        myAnimator.SetTrigger("Death");
+        //agent.speed = 0;
+        agent.isStopped = true;
+        StartCoroutine(DeathCorrutine());
+    }
+
+    public IEnumerator DeathCorrutine()
+    {
+        yield return new WaitForSeconds(timeToDie);
         Destroy(gameObject);
     }
 }
